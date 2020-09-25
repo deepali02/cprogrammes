6caed42b3b66c2edd594f17fa5e82639bee2bf0d
unknown <deepali.sharma0212@gmaial.comgit config --global user.email deepali.sharma0212@gmail.com>
2020-09-25 18:14:37 +0530

    File added

---
+++ DESIGN1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int i, j;
+clrscr();
+for (i=1; i<=4; i++)
+{
+for (j=1; j<=3; j++)
+{
+printf("%d\t", i);
+}
+printf("\n");
+}
+getch();
+return 0;
+}
